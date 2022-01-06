49a20940a1532079e1dcc8ca87b5c1c4e6fb7bf7
Nikolay <forsh_nikolay@mail.ru>
2022-01-06 19:33:45 +0300

    Lesson 3 fix bugs

--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -30,7 +30,7 @@ public class UserServiceTest {
     static {
         // Only for postgres driver logging
         // It uses java.util.logging and logged via jul-to-slf4j bridge
-        SLF4JBridgeHandler.install();
+         SLF4JBridgeHandler.install();
     }
 
     @Autowired
